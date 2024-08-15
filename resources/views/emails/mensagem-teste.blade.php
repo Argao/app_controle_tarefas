b24f0500b02d77e3b485ff69644572f091cc9c4e
argao <jgabrielaragao@gmail.com>
2024-08-15 13:07:40 -0300

    Config reset senha mail

--- resources/views/emails/mensagem-teste.blade.php
+++ resources/views/emails/mensagem-teste.blade.php
@@ -3,8 +3,16 @@
 
 O corpo da mensagem.
 
+- Lista 1
+- Lista 2
+- Lista 3
+
 @component('mail::button', ['url' => ''])
-Texto do botão
+Texto do botão 1
+@endcomponent
+
+@component('mail::button', ['url' => ''])
+    Texto do botão 2
 @endcomponent
 
 Obrigado,<br>
